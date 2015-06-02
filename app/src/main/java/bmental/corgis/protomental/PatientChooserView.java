6f79151c4fa7fef7197f0b138e950e96b370e52f
NateFudenberg <cyreal123@gmail.com>
2015-06-02 17:23:01 -0500

    Finally got shared prefs working right

--- app/src/main/java/bmental/corgis/protomental/PatientChooserView.java
+++ app/src/main/java/bmental/corgis/protomental/PatientChooserView.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Set;
 
 /**
  * Created by Folly on 6/1/2015.
@@ -16,13 +19,15 @@ public class PatientChooserView extends CorgiView{
         setContentView(R.layout.activity_patient_view);
 
 
-//        SharedPreferences stored = getSharedPreferences("prefs", 0);
-//        stored.get
+        SharedPreferences stored = getSharedPreferences("prefs", 0);
+        stored.getString("current_patient", null);
 
         findViewById(R.id.select_patient).setOnClickListener(proceedWithChosen);
         Spinner chooser = (Spinner) findViewById(R.id.spin_choice);
 
-        String[] meow = {"Patient1", "Patient2", "Patient3"};
+//        String[] meow = {"Patient1", "Patient2", "Patient3"};
+        Set<String> temp = stored.getStringSet("patients", null);
+        String[] meow = temp.toArray(new String[temp.size()]);
 
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_for_patients, meow);
@@ -37,6 +42,14 @@ public class PatientChooserView extends CorgiView{
         proceedWithChosen = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                SharedPreferences stored = getSharedPreferences("prefs", 0);
+
+                TextView very = (TextView) ((Spinner)findViewById(R.id.spin_choice)).getSelectedView();
+                SharedPreferences.Editor e = stored.edit();
+                e.putString("current_patient", very.getText().toString());
+
+                e.commit();
+
                 Intent change = new Intent(getApplicationContext(), PatientView.class);
 
                 startActivity(change);
