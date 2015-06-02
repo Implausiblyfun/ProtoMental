5cf73c1a0a5361058e49535441ff1a221956efd6
NateFudenberg <cyreal123@gmail.com>
2015-06-02 16:40:26 -0500

    Spinner update

--- app/src/main/java/bmental/corgis/protomental/PatientChooserView.java
+++ app/src/main/java/bmental/corgis/protomental/PatientChooserView.java
@@ -1,8 +1,11 @@
 package bmental.corgis.protomental;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by Folly on 6/1/2015.
@@ -12,7 +15,22 @@ public class PatientChooserView extends CorgiView{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_view);
 
+
+//        SharedPreferences stored = getSharedPreferences("prefs", 0);
+//        stored.get
+
         findViewById(R.id.select_patient).setOnClickListener(proceedWithChosen);
+        Spinner chooser = (Spinner) findViewById(R.id.spin_choice);
+
+        String[] meow = {"Patient1", "Patient2", "Patient3"};
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_for_patients, meow);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        chooser.setAdapter(adapter);
+
     }
     private final View.OnClickListener proceedWithChosen;
     {
